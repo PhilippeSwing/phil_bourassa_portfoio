2ee9bcbaf7e3bcb92e196980b32638121f674c4c
Phil <bourassa.phil@gmail.com>
2018-09-17 13:23:37 -0400

    Added typedjs. Need to add options and fix text styling. Next: Contact Section.

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello
-      </div>
-    )
-  }
-}
+$(function () {
+  // get the value of the bottom of the header-top element by adding the offset of that element plus its height, set it as a variable
+  let navBottom = $('.header-nav').offset().top + $('.header-nav').height();
 
-ReactDOM.render(<App />, document.getElementById('app'));
+  // on scroll, 
+  $(window).on('scroll', function () {
+
+    // we round here to reduce a little workload
+    let stop = Math.round($(window).scrollTop());
+
+    if (stop > navBottom) {
+      $('.header-nav').addClass('nav-scroll');
+    } else {
+      $('.header-nav').removeClass('nav-scroll');
+    }
+  });
+
+
+
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 20
+  });
+
+
+
+
+
+
+  // var options = {
+  // strings: ["Hi there, my name is", " Phil"],
+  // typeSpeed: 40
+
+  // var typed = new Typed(".hero-text__intro", options);
+
+  // var typed = new Typed('.hero-text__intro', {
+  // strings: ["Hi there, my name is", " Phil"],
+  // typeSpeed: 30
+  // });
+
+});
