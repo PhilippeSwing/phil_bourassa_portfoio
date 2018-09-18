09cde64752464dc2012d2422532dd7327efda17c
Phil <bourassa.phil@gmail.com>
2018-09-17 20:54:36 -0400

    Completed Contact and Footer sections.

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,42 +1,27 @@
 
 $(function () {
   // get the value of the bottom of the header-top element by adding the offset of that element plus its height, set it as a variable
-  let navBottom = $('.header-nav').offset().top + $('.header-nav').height();
+  // let navBottom = $('.header-nav').offset().top + $('.header-nav').height();
 
-  // on scroll, 
-  $(window).on('scroll', function () {
+  // // on scroll, 
+  // $(window).on('scroll', function () {
 
-    // we round here to reduce a little workload
-    let stop = Math.round($(window).scrollTop());
+  //   // we round here to reduce a little workload
+  //   let stop = Math.round($(window).scrollTop());
 
-    if (stop > navBottom) {
-      $('.header-nav').addClass('nav-scroll');
-    } else {
-      $('.header-nav').removeClass('nav-scroll');
-    }
-  });
-
-
+  //   if (stop > navBottom) {
+  //     $('.header-nav').addClass('nav-scroll');
+  //   } else {
+  //     $('.header-nav').removeClass('nav-scroll');
+  //   }
+  // });
 
   var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
-    typeSpeed: 20
+    typeSpeed: 40,
+    backSpeed: 20,
+    // loop: true,
+    showCursor: false,
   });
-
-
-
-
-
-
-  // var options = {
-  // strings: ["Hi there, my name is", " Phil"],
-  // typeSpeed: 40
-
-  // var typed = new Typed(".hero-text__intro", options);
-
-  // var typed = new Typed('.hero-text__intro', {
-  // strings: ["Hi there, my name is", " Phil"],
-  // typeSpeed: 30
-  // });
 
 });
