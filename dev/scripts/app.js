ab96fcfa842712c911789430ca68061e6b4af0dd
Phil <bourassa.phil@gmail.com>
2018-09-23 12:03:52 -0400

    Removed annoying 900 time deay text from type.js that displays on page load.

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,94 +1,77 @@
-$(function () {
-  $(this).scrollTop(0);
-  // get the value of the bottom of the header-top element by adding the offset of that element plus its height, set it as a variable
-  // let navBottom = $('.header-nav').offset().top + $('.header-nav').height();
+$(function() {
+    $(this).scrollTop(0);
 
-  // // on scroll, 
-  // $(window).on('scroll', function () {
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        typeSpeed: 85,
+        backSpeed: 20,
+        // loop: true,
+        showCursor: false,
+    });
 
-  //   // we round here to reduce a little workload
-  //   let stop = Math.round($(window).scrollTop());
+    // SMOOTH SCROLL STARTS
+    // $('.header-nav__item__link').smoothScroll();
+    $('.header-nav__item__link, .arrow-link, .hero-text__portfolio-button').click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1200);
+    });
+    // SMOOTH SCROLL ENDS
 
-  //   if (stop > navBottom) {
-  //     $('.header-nav').addClass('nav-scroll');
-  //   } else {
-  //     $('.header-nav').removeClass('nav-scroll');
-  //   }
-  // });
+    // FLICKITY STARTS
+    $('.blog-post-container').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+        wrapAround: true,
+    });
+    // FLICKITY ENDS
 
-  var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 40,
-    backSpeed: 20,
-    // loop: true,
-    showCursor: false,
-  });
+    // MENU DISPLAY STARTS
+    $('.menu-icon').on('click', function() {
+        $('.header-top-container').toggleClass('menu-display');
+    });
+    // MENU DISPLAY ENDS
 
-  // SMOOTH SCROLL STARTS
-  // $('.header-nav__item__link').smoothScroll();
-  $('.header-nav__item__link, .arrow-link, .hero-text__portfolio-button').click(function (e) {
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1200);
-  });
-  // SMOOTH SCROLL ENDS
+    // Projects Fade In STARTS
+    new WOW().init();
+    // Projects Fade In ENDS
 
-  // FLICKITY STARTS
-  $('.blog-post-container').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true,
-    wrapAround: true,
-  });
-  // FLICKITY ENDS
+    // ========================
+    // ========================
+    // BEN'S BAGELS!!!
+    const pressed = [];
+    const code = "bensbagels";
+    // let frameCount = 0;
 
-  // MENU DISPLAY STARTS
-  $('.menu-icon').on('click', function () {
-    $('.header-top-container').toggleClass('menu-display');
-    // $(this).toggleClass('hide');
-    // $('close-icon').toggleClass('hide');
-  });
-  // MENU DISPLAY ENDS
-
-  // Projects Fade In STARTS
-  new WOW().init();
-  // Projects Fade In ENDS
-
-  // ========================
-  // ========================
-  // BEN'S BAGELS!!!
-  const pressed = [];
-  const code = "bensbagels";
-  // let frameCount = 0;
-
-  const finishbagels = () => {
-    let bagel = document.getElementById("bagel-container");
-    bagel.className = "hide";
-  }
-
-  const trigger = () => {
-    setTimeout(
-      function () {
-        finishbagels();
-      }, 10000);
-  }
-
-  const startbagels = () => {
-    let bagel = document.getElementById("bagel-container");
-    bagel.className = "show";
-    trigger();
-
-  }
-
-  window.addEventListener("keyup", function (e) {
-    pressed.push(e.key);
-    pressed.splice(-code.length - 1, pressed.length - code.length);
-
-    if (pressed.join("").includes(code)) {
-      startbagels();
+    const finishbagels = () => {
+        let bagel = document.getElementById("bagel-container");
+        bagel.className = "hide";
     }
-  });
-  // BEN'S BAGELS!!!
+
+    const trigger = () => {
+        setTimeout(
+            function() {
+                finishbagels();
+            }, 10000);
+    }
+
+    const startbagels = () => {
+        let bagel = document.getElementById("bagel-container");
+        bagel.className = "show";
+        trigger();
+
+    }
+
+    window.addEventListener("keyup", function(e) {
+        pressed.push(e.key);
+        pressed.splice(-code.length - 1, pressed.length - code.length);
+
+        if (pressed.join("").includes(code)) {
+            startbagels();
+        }
+    });
+    // BEN'S BAGELS!!!
 
 });
